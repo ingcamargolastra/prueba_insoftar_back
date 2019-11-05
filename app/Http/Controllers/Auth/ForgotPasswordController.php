84ee264ed186808b3fd70d3dfc99be679665129a
Desarrollo Tamaca <desarrollotamaca@gmail.com>
2019-11-05 15:58:23 -0500

    Commit Inicial

---
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace PruebaInsoftar\Http\Controllers\Auth;
+
+use PruebaInsoftar\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+
+class ForgotPasswordController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller is responsible for handling password reset emails and
+    | includes a trait which assists in sending these notifications from
+    | your application to your users. Feel free to explore this trait.
+    |
+    */
+
+    use SendsPasswordResetEmails;
+}
